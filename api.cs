c0bfbe92e9d701cd1352548935a63a25b9b64d75
daniel-andersen <daniel.andersen@unity3d.com>
2023-05-28 03:30:20 +0200

    init

---
+++ api.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using Unity.Burst;
+
+[BurstCompile]
+public class API
+{
+    [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+    static extern System.IntPtr tree_sitter_c_sharp();
+    
+    // burst wrapper
+    [BurstCompile]
+    public static System.IntPtr TreeSitterCSharp() => tree_sitter_c_sharp();
+}
